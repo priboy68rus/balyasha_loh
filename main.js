ee948d0f2f3807abbec85b9136bf4667e2abda2e
john <mac@MacBook-Air-mac.lan>
2014-12-02 05:51:16 +0300

    Added marina

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
 		var cur = 0;
 		$('.sof').click(function() {
-			if (cur === 1)
+			if (cur == 1)
 			{
 				$('p').fadeOut(400, function(){
-					$('p').html('Who is Balyasha?');
+					$('p').html('Who is Marina?');
 				});
 				
 				$('p').fadeIn(400);
 
-				cur = 0;
+				cur = 2;
 			}
-			else
+			else if (cur == 0)
 			{
 				$('p').fadeOut(400, function (){
 					$('p').html('Balyasha loshara');
@@ -19,6 +19,24 @@ $(document).ready(function() {
 			
 				$('p').fadeIn(400);
 				cur = 1;
+			}
+			else if (cur == 2)
+			{
+				$('p').fadeOut(400, function (){
+					$('p').html('Marina is cool');
+				});
+			
+				$('p').fadeIn(400);
+				cur = 3;
+			}
+			else if (cur == 3)
+			{
+				$('p').fadeOut(400, function (){
+					$('p').html('Who is Balyasha?');
+				});
+			
+				$('p').fadeIn(400);
+				cur = 0;
 			}
 
 		});
